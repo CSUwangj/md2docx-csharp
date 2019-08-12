18e19437a3d5cc0347a26cf2aa33664f15d8ff34
CSUwangj <CSUwangj@protonmail.com>
2019-08-12 16:20:06 +0800

    feat: init project

---
+++ md2docx/md2docx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace md2docx
+{
+    class Md2Docx
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello Md2Docx!");
+        }
+    }
+}
