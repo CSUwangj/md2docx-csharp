5dbd66fd9c293f4930676c34ad080b36b948a677
CSUwangj <CSUwangj@protonmail.com>
2019-08-12 19:05:51 +0800

    feat: parse yaml for base information

--- md2docx/md2docx.cs
+++ md2docx/md2docx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Toolkit.Parsers.Markdown;
+using Microsoft.Toolkit.Parsers.Markdown.Blocks;
 
 namespace md2docx
 {
@@ -6,7 +8,60 @@ namespace md2docx
     {
         static void Main(String[] args)
         {
-            Console.WriteLine("Hello Md2Docx!");
+            string md = System.IO.File.ReadAllText("test.md");
+            Console.WriteLine(md);
+
+            MarkdownDocument mddoc = new MarkdownDocument();
+            mddoc.Parse(md);
+
+            string name = "";
+            string id = "";
+            string teacher = "";
+            string department = "";
+            string title = "";
+            string clas = "";
+
+            foreach (var element in mddoc.Blocks)
+            {
+                if (element is YamlHeaderBlock yaml)
+                {
+                    name = yaml.Children["name"];
+                    id = yaml.Children["id"];
+                    teacher = yaml.Children["teacher"];
+                    department = yaml.Children["department"];
+                    title = yaml.Children["title"];
+                    clas = yaml.Children["class"];
+                }
+                else if (element is ParagraphBlock para)
+                {
+                    foreach (var e in para.Inlines)
+                    {
+                        Console.WriteLine($"Para({e.GetType()}) {e.ToString()}");
+                    }
+                }
+                else if (element is ListBlock list) //我就操了为什么list里面可以放block，我丢
+                {
+                    foreach (var e in list.Items)
+                    {
+                        Console.WriteLine($"wtf({e.GetType()}) {e.Blocks[0].ToString()}");
+                    }
+                }
+                else if (element is HeaderBlock head)
+                {
+                    Console.Write($"H({head.HeaderLevel},)");
+                    foreach (var e in head.Inlines)
+                    {
+                        Console.Write($"[{e.GetType()}, {e.ToString()}]");
+                    }
+                    Console.WriteLine("");
+                }
+            }
+            Console.WriteLine($"name {name}");
+            Console.WriteLine($"id {id}");
+            Console.WriteLine($"teacher {teacher}");
+            Console.WriteLine($"department {department}");
+            Console.WriteLine($"title {title}");
+            Console.WriteLine($"class {clas}");
         }
     }
 }
